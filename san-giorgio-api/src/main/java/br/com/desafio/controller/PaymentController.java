49554613107550ee6c018536fb5158aeee4fcd31
scroutchy <stef.crombez@gmail.com>
2024-09-20 01:35:39 +0200

    feat: Controller improvements

--- san-giorgio-api/src/main/java/br/com/desafio/controller/PaymentController.java
+++ san-giorgio-api/src/main/java/br/com/desafio/controller/PaymentController.java
@@ -3,8 +3,6 @@ package br.com.desafio.controller;
 import br.com.desafio.domain.mapper.PaymentMapper;
 import br.com.desafio.domain.model.PaymentModel;
 import br.com.desafio.domain.usecase.ConfirmPaymentUseCase;
-import br.com.desafio.exception.ClientNotFoundException;
-import br.com.desafio.exception.PaymentItemNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.OK;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,23 +19,13 @@ public class PaymentController {
     private final PaymentMapper paymentMapper;
     private final ConfirmPaymentUseCase confirmPaymentUseCase;
 
-    @PostMapping(path = "/api/payment")
-    public ResponseEntity<?> setPayment(@Valid @RequestBody Payment request) {
-        Payment responsePayment;
+    @PostMapping(path = "/api/payments")
+    public ResponseEntity<Payment> setPayments(@Valid @RequestBody Payment request) {
+        PaymentModel paymentModel = paymentMapper.toPaymentModel(request);
+        PaymentModel updatedPaymentModel = confirmPaymentUseCase.confirm(paymentModel);
 
-        try {
-            PaymentModel paymentModel = paymentMapper.toPaymentModel(request);
-
-            PaymentModel updatedPaymentModel = confirmPaymentUseCase.confirm(paymentModel);
-
-            responsePayment = paymentMapper.toPayment(updatedPaymentModel);
-
-            return ResponseEntity.status(OK).body(responsePayment);
-        } catch (ClientNotFoundException | PaymentItemNotFoundException e) {
-            return ResponseEntity.status(NOT_FOUND).body(new ErrorResponse(e.getMessage()));
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
-        }
+        Payment responsePayment = paymentMapper.toPayment(updatedPaymentModel);
+        return ResponseEntity.status(OK).body(responsePayment);
 
     }
 }
---
+++ san-giorgio-api/src/test/java/br/com/desafio/controller/PaymentControllerTest.java
@@ -0,0 +1,115 @@
+package br.com.desafio.controller;
+
+import br.com.desafio.domain.mapper.PaymentMapper;
+import br.com.desafio.domain.model.PaymentModel;
+import br.com.desafio.domain.usecase.ConfirmPaymentUseCase;
+import br.com.desafio.exception.ClientNotFoundException;
+import br.com.desafio.exception.PaymentItemNotFoundException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PaymentController.class)
+public class PaymentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PaymentMapper paymentMapper;
+
+    @MockBean
+    private ConfirmPaymentUseCase confirmPaymentUseCase;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Payment paymentRequest;
+    private PaymentModel paymentModel;
+    private PaymentModel updatedPaymentModel;
+    private Payment responsePayment;
+
+    @BeforeEach
+    public void setUp() {
+        paymentRequest = new Payment();
+        paymentModel = new PaymentModel();
+        updatedPaymentModel = new PaymentModel();
+        responsePayment = new Payment();
+    }
+
+    @Test
+    void testSetPayments_Success() throws Exception {
+        when(paymentMapper.toPaymentModel(paymentRequest)).thenReturn(paymentModel);
+        when(confirmPaymentUseCase.confirm(paymentModel)).thenReturn(updatedPaymentModel);
+        when(paymentMapper.toPayment(updatedPaymentModel)).thenReturn(responsePayment);
+
+        mockMvc.perform(post("/api/payments")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(paymentRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON));
+
+        verify(paymentMapper).toPaymentModel(paymentRequest);
+        verify(confirmPaymentUseCase).confirm(paymentModel);
+        verify(paymentMapper).toPayment(updatedPaymentModel);
+    }
+
+    @Test
+    void testSetPayments_ClientNotFound() throws Exception {
+        when(paymentMapper.toPaymentModel(paymentRequest)).thenReturn(paymentModel);
+        when(confirmPaymentUseCase.confirm(paymentModel)).thenThrow(new ClientNotFoundException("Client not found"));
+
+        mockMvc.perform(post("/api/payments")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(paymentRequest)))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(APPLICATION_JSON));
+        // .andExpect(content().json("{\"message\":\"Client not found\"}"));
+
+        verify(paymentMapper).toPaymentModel(paymentRequest);
+        verify(confirmPaymentUseCase).confirm(paymentModel);
+    }
+
+    @Test
+    void testSetPayments_PaymentItemNotFound() throws Exception {
+        when(paymentMapper.toPaymentModel(paymentRequest)).thenReturn(paymentModel);
+        when(confirmPaymentUseCase.confirm(paymentModel)).thenThrow(new PaymentItemNotFoundException("Payment item not found"));
+
+        mockMvc.perform(post("/api/payments")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(paymentRequest)))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(APPLICATION_JSON));
+        //.andExpect(content().json("{\"message\":\"Payment item not found\"}"));
+
+        verify(paymentMapper).toPaymentModel(paymentRequest);
+        verify(confirmPaymentUseCase).confirm(paymentModel);
+    }
+
+    @Test
+    void testSetPayments_InvalidArgument() throws Exception {
+        when(paymentMapper.toPaymentModel(paymentRequest)).thenReturn(paymentModel);
+        when(confirmPaymentUseCase.confirm(paymentModel)).thenThrow(new IllegalArgumentException("Invalid payment"));
+
+        mockMvc.perform(post("/api/payments")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(paymentRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(APPLICATION_JSON));
+        //.andExpect(content().json("{\"message\":\"Invalid payment\"}"));
+
+        verify(paymentMapper).toPaymentModel(paymentRequest);
+        verify(confirmPaymentUseCase).confirm(paymentModel);
+    }
+}
