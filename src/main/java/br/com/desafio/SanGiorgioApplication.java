f3c0c8d266e5e22d53800f323bc6f427e3e355b2
scroutchy <stef.crombez@gmail.com>
2025-01-24 10:24:57 +0100

    feat: Sending to SQS queue

--- src/main/java/br/com/desafio/SanGiorgioApplication.java
+++ src/main/java/br/com/desafio/SanGiorgioApplication.java
@@ -1,10 +1,13 @@
 package br.com.desafio;
 
+import br.com.desafio.domain.config.AwsCredentialsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
+@EnableConfigurationProperties(AwsCredentialsConfig.class)
 public class SanGiorgioApplication {
     public static void main(String[] args) {
         SpringApplication.run(SanGiorgioApplication.class, args);
