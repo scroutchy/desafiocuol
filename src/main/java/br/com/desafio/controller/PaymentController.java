1eef9d9eeb87060376c12db5df91f1d3c95716ab
scroutchy <stef.crombez@gmail.com>
2025-01-24 22:13:00 +0100

    feat: Adding logs

--- src/main/java/br/com/desafio/controller/PaymentController.java
+++ src/main/java/br/com/desafio/controller/PaymentController.java
@@ -5,6 +5,7 @@ import br.com.desafio.domain.model.Payment;
 import br.com.desafio.domain.usecase.ConfirmPaymentUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class PaymentController {
@@ -21,10 +23,14 @@ public class PaymentController {
 
     @PostMapping(path = "/api/payments")
     public ResponseEntity<PaymentApiDto> setPayments(@Valid @RequestBody PaymentApiDto request) {
+        log.debug("Received payment request: {}", request);
         Payment payment = paymentMapper.toPayment(request);
+        log.debug("Mapped PaymentApiDto to Payment: {}", payment);
         Payment updatedPayment = confirmPaymentUseCase.confirm(payment);
+        log.debug("Payment processed successfully: {}", updatedPayment);
 
         PaymentApiDto responsePayment = paymentMapper.toPaymentApiDto(updatedPayment);
+        log.debug("Mapped updated Payment to PaymentApiDto: {}", responsePayment);
         return ResponseEntity.status(OK).body(responsePayment);
 
     }
